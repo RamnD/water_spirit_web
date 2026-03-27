9b6feaff2b80389af5e6ca1d4ce36402c5ab9671
GKey <thesuperboy112@gmail.com>
2026-03-27 11:07:33 +0300

    Add initial project files for Water Spirit WebGL build
    - Created HTML structure for the Unity Web Player with loading bar and warning banner.
    - Added CSS styles for the Unity container, canvas, loading bar, and logo.
    - Included PNG assets for logos and progress bars in both light and dark themes.
    - Implemented a web manifest for the application with metadata and icons.
    - Registered a service worker for offline capabilities.

---
+++ ServiceWorker.js
@@ -0,0 +1,33 @@
+const cacheName = "Ramnd-Water Spirit-0.0.1";
+const contentToCache = [
+    "Build/ws_build_0.0.1c.loader.js",
+    "Build/ws_build_0.0.1c.framework.js",
+    "Build/ws_build_0.0.1c.data",
+    "Build/ws_build_0.0.1c.wasm",
+    "TemplateData/style.css"
+
+];
+
+self.addEventListener('install', function (e) {
+    console.log('[Service Worker] Install');
+    
+    e.waitUntil((async function () {
+      const cache = await caches.open(cacheName);
+      console.log('[Service Worker] Caching all: app shell and content');
+      await cache.addAll(contentToCache);
+    })());
+});
+
+self.addEventListener('fetch', function (e) {
+    e.respondWith((async function () {
+      let response = await caches.match(e.request);
+      console.log(`[Service Worker] Fetching resource: ${e.request.url}`);
+      if (response) { return response; }
+
+      response = await fetch(e.request);
+      const cache = await caches.open(cacheName);
+      console.log(`[Service Worker] Caching new resource: ${e.request.url}`);
+      cache.put(e.request, response.clone());
+      return response;
+    })());
+});
